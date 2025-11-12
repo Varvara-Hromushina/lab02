86ad70ff3e3f9715f7def9e4437a4b9ee8309201
Varvara-Hromushina <varvara.hromushina@yandex.ru>
2025-11-12 20:24:29 +0300

    Apply Mozilla code style with clang-format

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 #include <string>
 
+
 // Основная функция, которая запрашивает имя пользователя и печатает приветствие
-int main() {
+// Main function that asks for user name and prints greeting
+int
+main()
+{
+>>>>>>> 1156b8d (Apply Mozilla code style with clang-format)
 std::string name;
 std::cout << "Enter your name: ";
 std::cin >> name;
